613da23b3e96bc389028dae4520ddd10b1ad473d
yasin abedini <yasinabedini1384@gmail.com>
2024-05-25 20:35:17 +0330

    feat - Add Test Api To Project

---
+++ ApiTest/ApiTest/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using ApiTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public List<User> Users { get; set; } = new List<User>
+        {
+            new User(1,"Yasin Abedini","09106966244"),
+            new User(2,"user test 1","09106966244"),
+            new User(3,"user test 2","09130258974"),
+            new User(4,"user test 3","09134569874"),
+            new User(5,"user test 4","09132587496"),
+        };
+
+        
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return Users;
+        }
+        
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return Users.FirstOrDefault(t=>t.Id==id);
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            return Ok("User Cerated SuccessFully...");
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody]User user)
+        {
+            return Ok("User Updated SuccessFully...");
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok("User Deleted SuccessFully...");
+        }
+    }
+}
---
+++ ApiTest/ApiTest/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiTest.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public User(int id, string userName, string phoneNumber)
+        {
+            Id = id;
+            UserName = userName;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}

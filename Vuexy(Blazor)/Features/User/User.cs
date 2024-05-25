e9e9087c8f7d77ba1a93af2c58679f16c80c8083
yasin abedini <yasinabedini1384@gmail.com>
2024-05-25 20:35:37 +0330

    fix - fix CSRF Bug

---
+++ Vuexy(Blazor)/Features/User/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Features.User;
+
+public class UserViewModel
+{
+    public string Id { get; set; }
+    public string UserName { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public UserViewModel(string id, string userName, string phoneNumber)
+    {
+        Id = id;
+        UserName = userName;
+        PhoneNumber = phoneNumber;
+    }
+}
